2343aa70e86fb758a829cf374182d013d244f907
Sergey Vinogradov <svvinogradov@alfabank.ru>
2017-11-27 16:24:11 +0400

    Correction of the ConsoleControlSample.WPF - added a dialog to start any process with arguments, by analogy with the ConsoleControlSample. Launch background workers at the start of the process. Refactoring.

---
+++ source/ConsoleControlSample.WPF/NewProcessDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace ConsoleControlSample.WPF
+{
+    /// <summary>
+    /// Interaction logic for NewProcessDialog.xaml
+    /// </summary>
+    public partial class NewProcessDialog : Window
+    {
+        public NewProcessDialog()
+        {
+            InitializeComponent();
+
+            ViewModel.AcceptCommand.Executed += new Apex.MVVM.CommandEventHandler(AcceptCommand_Executed);
+        }
+
+        /// <summary>
+        /// Handles the Executed event of the AcceptCommand control. 
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="args">The <see cref="Apex.MVVM.CommandEventArgs"/> instance containing the event data.</param>
+        void AcceptCommand_Executed(object sender, Apex.MVVM.CommandEventArgs args)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ source/ConsoleControlSample.WPF/NewProcessViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Apex.MVVM;
+
+namespace ConsoleControlSample.WPF
+{
+    /// <summary>
+    /// The New Process ViewModel.
+    /// </summary>
+    public class NewProcessViewModel : ViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NewProcessViewModel"/> class.
+        /// </summary>
+        public NewProcessViewModel()
+        {
+            AcceptCommand = new Command(() => { });
+        }
+
+        /// <summary>
+        /// Gets the accept command.
+        /// </summary>
+        public Command AcceptCommand
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets or sets program file name.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets program arguments.
+        /// </summary>
+        public string Args { get; set; }
+    }        
+}
